28963e11d40e4f5b67a885f94dfdac3fb9e34461
Matthew Phillips <phillipsm2@gmail.com>
2018-09-18 21:38:58 -0400

    Initial commit

---
+++ fly-out.js
@@ -0,0 +1,113 @@
+
+function template(label) {
+  return `
+    <style>
+      .menu {
+        display: none;
+      }
+
+      .menu.open {
+        display: block;
+      }
+
+      button {
+        cursor: pointer;
+        border: none;
+        font-size: 12px;
+      }
+    </style>
+    <div class="has-submenu">
+      <button type="button" aria-haspopup="true"
+        aria-expanded="false" class="has-submenu">${label}</button>
+      <slot class="menu"></slot>
+    </div>
+  `;
+}
+
+function notImplemented() {
+  throw new Error('Not yet implemented');
+}
+
+class FlyOut extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+
+    this._menu = null;
+    this._hasSetup = false;
+    this._isExpanded = false;
+  }
+
+  _setup() {
+    if(!this._hasSetup) {
+      this._hasSetup = true;
+
+      let label = this.getAttribute('label');
+      this.shadowRoot.innerHTML = template(label);
+
+      let root = this.shadowRoot;
+      this._menu = root.querySelector('.menu');
+      this._submenu = root.querySelector('.has-submenu');
+      this._btn = root.querySelector('button');
+    }
+  }
+
+  connectedCallback() {
+    this._setup();
+
+    if(this.hasAttribute('hover')) {
+      this._btn.addEventListener('mouseenter', this);
+      this._submenu.addEventListener('mouseleave', this);
+    } else {
+      this._btn.addEventListener('click', this);
+    }
+    this._btn.addEventListener('keyup', this);
+  }
+
+  disconnectedCallback() {
+    this._btn.removeEventListener('mouseenter', this);
+    this._btn.removeEventListener('click', this);
+    this._btn.removeEventListener('keyup', this);
+    this._submenu.removeEventListener('mouseleave', this);
+  }
+
+  handleEvent(ev) {
+    switch(ev.type) {
+      case 'click':
+        notImplemented();
+      case 'mouseenter':
+        this._makeExpanded();
+        break;
+      case 'mouseleave':
+        this._makeClosed();
+      case 'keyup':
+        let k = ev.keyCode;
+        if(k === 32 || k === 13) {
+          if(this._isExpanded) {
+            this._makeClosed();
+          } else {
+            this._makeExpanded();
+          }
+        }
+        break;
+    }
+  }
+
+  _makeExpanded() {
+    if(!this._isExpanded) {
+      this._menu.classList.add('open');
+      this._submenu.setAttribute('aria-expanded', 'true');
+      this._isExpanded = true;
+    }
+  }
+
+  _makeClosed() {
+    if(this._isExpanded) {
+      this._menu.classList.remove('open');
+      this._submenu.setAttribute('aria-expanded', 'false');
+      this._isExpanded = false;
+    }
+  }
+}
+
+customElements.define('fly-out', FlyOut);
